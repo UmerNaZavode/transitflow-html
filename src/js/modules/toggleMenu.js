63c6fd2fb721f317a0995db8d3a187a64c397eeb
gaba-work <malkocmax@gmail.com>
2025-07-04 12:51:16 +0300

    small changes

--- src/js/modules/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-export default function toggleMenu() {
-  const burger = document.querySelector(".burger")
-  const menu = document.querySelector(".header-main")
-  burger.addEventListener("click", function() {
-    menu.classList.toggle("active")
-  })
-
-}
